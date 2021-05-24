6d07bb8837e49c22920df3a926dc3f20896d01d1
oscarnav <65473298+oscarnav@users.noreply.github.com>
2021-05-24 03:14:43 -0600

    Update cliente.cpp

--- src/Sockets/cliente.cpp
+++ src/Sockets/cliente.cpp
@@ -8,12 +8,10 @@
 #include<netdb.h>
 void enviarMensaje(char* mensaje, char* host){
   
-  string host;
-  int puerto;
-  
-  struct sockaddr_in cliente; //Declaración de la estructura con información para la conexión
-  struct hostent *servidor; //Declaración de la estructura con información del host
-  servidor = gethostbyname(host); //Asignacion
+
+  struct sockaddr_in cliente;
+  struct hostent *servidor;
+  servidor = gethostbyname(host); 
   if(servidor == NULL)
   { //Comprobación
     printf("Host erróneo\n");
@@ -21,27 +19,23 @@ void enviarMensaje(char* mensaje, char* host){
   }
   int puerto, conexion;
   char buffer[100];
-  conexion = socket(AF_INET, SOCK_STREAM, 0); //Asignación del socket
-  puerto=(puerto); //conversion del argumento
-  bzero((char *)&cliente, sizeof((char *)&cliente)); //Rellena toda la estructura de 0's
-        //La función bzero() es como memset() pero inicializando a 0 todas la variables
-  cliente.sin_family = AF_INET; //asignacion del protocolo
-  cliente.sin_port = htons(puerto); //asignacion del puerto
+  conexion = socket(AF_INET, SOCK_STREAM, 0); 
+  bzero((char *)&cliente, sizeof((char *)&cliente)); 
+        
+  cliente.sin_family = AF_INET; 
+  cliente.sin_port = htons(puerto); 
   bcopy((char *)servidor->h_addr, (char *)&cliente.sin_addr.s_addr, sizeof(servidor->h_length));
-  //bcopy(); copia los datos del primer elemendo en el segundo con el tamaño máximo 
-  //del tercer argumento.
+ 
 
 
-  //cliente.sin_addr = *((struct in_addr *)servidor->h_addr); //<--para empezar prefiero que se usen
-  //inet_aton(argv[1],&cliente.sin_addr); //<--alguna de estas dos funciones
-  if(connect(conexion,(struct sockaddr *)&cliente, sizeof(cliente)) < 0)
-  { //conectando con el host
+  
+  if(connect(conexion,(struct sockaddr *)&cliente, sizeof(cliente)) < 0){ 
     printf("Error conectando con el host\n");
     close(conexion);
     return 1;
   }
   printf("Conectado con %s:%d\n",inet_ntoa(cliente.sin_addr),htons(cliente.sin_port));
-  //inet_ntoa(); está definida en <arpa/inet.h>
+  
   
   //buffer es el mensaje a  enviar
   buffer = mensaje;
