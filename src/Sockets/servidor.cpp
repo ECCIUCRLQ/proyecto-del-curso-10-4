6c2daeddfa180224b6bf46b9a2e87bc733eb9a1b
oscarnav <65473298+oscarnav@users.noreply.github.com>
2021-05-24 03:11:37 -0600

    Update servidor.cpp

--- src/Sockets/servidor.cpp
+++ src/Sockets/servidor.cpp
@@ -9,30 +9,29 @@
 void recibirMensaje(int puerto){
   
   
-  int conexion_servidor, conexion_cliente; //declaramos las variables
-  socklen_t longc; //Debemos declarar una variable que contendrá la longitud de la estructura
+  int conexion_servidor, conexion_cliente;
+  socklen_t longc; 
   struct sockaddr_in servidor, cliente;
-  char buffer[100]; //Declaramos una variable que contendrá los mensajes que recibamos
+  char buffer[100]; 
 
 
-  conexion_servidor = socket(AF_INET, SOCK_STREAM, 0); //creamos el socket
-  bzero((char *)&servidor, sizeof(servidor)); //llenamos la estructura de 0's
-  servidor.sin_family = AF_INET; //asignamos a la estructura
+  conexion_servidor = socket(AF_INET, SOCK_STREAM, 0); 
+  bzero((char *)&servidor, sizeof(servidor));
+  servidor.sin_family = AF_INET; 
   servidor.sin_port = htons(puerto);
-  servidor.sin_addr.s_addr = INADDR_ANY; //esta macro especifica nuestra dirección
+  servidor.sin_addr.s_addr = INADDR_ANY; 
   
   
-  if(bind(conexion_servidor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0)
-  { //asignamos un puerto al socket
+  if(bind(conexion_servidor, (struct sockaddr *)&servidor, sizeof(servidor)) < 0){ 
     printf("Error al asociar el puerto a la conexion\n");
     close(conexion_servidor);
     return 1;
   }
   
-  listen(conexion_servidor, 3); //Estamos a la escucha
+  listen(conexion_servidor, 3);
   printf("A la escucha en el puerto %d\n", ntohs(servidor.sin_port));
-  longc = sizeof(cliente); //Asignamos el tamaño de la estructura a esta variable
-  conexion_cliente = accept(conexion_servidor, (struct sockaddr *)&cliente, &longc); //Esperamos una conexion
+  longc = sizeof(cliente); 
+  conexion_cliente = accept(conexion_servidor, (struct sockaddr *)&cliente, &longc);
  
 
  if(conexion_cliente<0)
@@ -44,15 +43,13 @@ void recibirMensaje(int puerto){
   
   printf("Conectando con %s:%d\n", inet_ntoa(cliente.sin_addr),htons(cliente.sin_port));
   
-  if(recv(conexion_cliente, buffer, 100, 0) < 0)
-  { //Comenzamos a recibir datos del cliente
-    //Si recv() recibe 0 el cliente ha cerrado la conexion. Si es menor que 0 ha habido algún error.
+  if(recv(conexion_cliente, buffer, 100, 0) < 0){ 
+   
     printf("Error al recibir los datos\n");
     close(conexion_servidor);
     return 1;
   }
-  else
-  {
+  else{
     printf("%s\n", buffer);
     bzero((char *)&buffer, sizeof(buffer));
     send(conexion_cliente, "Recibido\n", 13, 0);
