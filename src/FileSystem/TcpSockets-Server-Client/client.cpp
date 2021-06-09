251f434cbbfb332d5b2e004dcb6466d93d99ec73
Oscar Navarro <onav1411@gmail.com>
2021-06-09 12:42:12 -0600

    Se agrega interaccion y nuevos comandos

--- src/FileSystem/TcpSockets-Server-Client/client.cpp
+++ src/FileSystem/TcpSockets-Server-Client/client.cpp
@@ -6,12 +6,19 @@ int main(int argc, char const *argv[]) {
   TcpClient client;
   Socket& socket = client.connect("127.0.0.1", "8081");
   std::string comando;
-  comando = "cB95549";
-  socket << comando;
-  socket.send();
-  std::string hileraRecibida;
-  socket.readLine(hileraRecibida);
-  std::cout << "Se recibio de servidor: " << hileraRecibida << std::endl;
+
+  
+    comando = "";
+    std::cout << "Ingrese comando: " << std::endl;
+    std::cin >> comando;
+    socket << comando;
+    socket.send();
+    std::string hileraRecibida;
+    socket.readLine(hileraRecibida);
+    std::cout << "Se recibio de servidor: " << hileraRecibida << std::endl;
+  
+  //comando = "cB95549";
+  
 
 
   socket.close();
