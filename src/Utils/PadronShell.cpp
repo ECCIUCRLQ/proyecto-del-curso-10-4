4a864acc54d9a21ee4228c9a916f7d591dd04024
Steve Moon <56247331+sungjaemoonUCR@users.noreply.github.com>
2021-07-05 03:17:59 -0600

    adding log history

---
+++ src/Utils/PadronShell.cpp
@@ -0,0 +1,111 @@
+#include "PadronShell.hpp"
+
+#include <iostream>
+
+PadronShell::PadronShell(PadronClient& padronClient) : padronClient(padronClient) {
+}
+
+PadronShell::~PadronShell() {
+}
+
+void PadronShell::shell() {
+  std::string line;
+  std::cout << "[Padron Client]: ";
+  while (std::getline(std::cin, line)) {
+    this->parse(line);
+    std::cout << "[Padron Client]: ";
+  }
+}
+
+void PadronShell::parse(const std::string& input) {
+  std::string command;
+  std::string filepath;
+
+  // Search for the first space
+  size_t pos = input.find(' ');
+
+  // Gets the command input
+  command = input.substr(0, pos);
+
+  // Create Command
+ 
+  // Update Code
+  if (command.compare(UPDATECODE_COMMAND) == 0) {
+    std::string carnet = input.substr(pos + 1,pos+7);
+   // std::string code = input.substr(pos+9, pos+29);
+    std::string hasheado = "vm5960sl6059ot06pdof";
+    std::cout << "Your Carnet : " << carnet << " associated with verificaition code:"<<hasheado<<std::endl;
+    this->updateCode(carnet, hasheado);
+    return;
+  }
+  // Verify Code
+  if (command.compare(VERIFYCODE_COMMAND) == 0) {
+    std::string carnet = input.substr(pos + 1,pos+7);
+   // std::string code = input.substr(pos+9, pos+29);
+    std::string hasheado = "vm5960sl6059ot06pdof";
+    std::cout << "Your Carnet : " << carnet << " associated with verificaition code:"<<hasheado<<std::endl;
+    this->verifyCode(carnet, hasheado);
+    return;
+  }
+  if (command.compare(VERIFYCARNET_COMMAND) == 0) {
+    std::string carnet = input.substr(pos + 1,pos+7);
+    std::cout << "Your Carnet : " << carnet;
+    this->verifyCarnet(carnet);
+    return;
+  }
+    if (command.compare(UPDATEVOTE_COMMAND) == 0) {
+    std::string carnet = input.substr(pos + 1,pos+7);
+    std::cout << "Your Carnet : " << carnet;
+    this->updateVote(carnet);
+    return;
+  }
+
+
+  // Print Disk
+  if (command.compare(PRINT_COMMAND) == 0) {
+    this->printDisk();
+    return;
+  }
+
+  std::cout << "Error: invalid command" << std::endl;
+  return;
+}
+
+
+
+
+void PadronShell::updateCode(const std::string& filepath,std::string codigo) {
+  if (this->padronClient.updateCode(filepath,codigo)) {
+    std::cout << "updateCode: code updated in padron successfully" << std::endl;
+  } else {
+    std::cout << "updateCode: not updated successfully" << std::endl;
+  }
+}
+void PadronShell::verifyCode(const std::string& filepath,std::string codigo) {
+  if (this->padronClient.verifyCode(filepath,codigo)) {
+    std::cout << "verifyCode: code verified sucessfully" << std::endl;
+  } else {
+    std::cout << "verifyCode: invalid code" << std::endl;
+  }
+}
+void PadronShell::verifyCarnet(const std::string& filepath) {
+  if (this->padronClient.verifyCarnet(filepath)) {
+    std::cout << "verifyCarnet: valid code" << std::endl;
+  } else {
+    std::cout << "verifyCarnet: invalid code" << std::endl;
+  }
+}
+void PadronShell::updateVote(const std::string& filepath) {
+  if (this->padronClient.updateVote(filepath)) {
+    std::cout << "updateVote: vote updated successfully" << std::endl;
+  } else {
+    std::cout << "updateVote: unsuccessful update" << std::endl;
+  }
+}
+void PadronShell::printDisk() {
+  if (this->padronClient.printDisk()) {
+    this->padronClient.fs->printHD();
+  } else {
+    std::cout << "PrintDisk: could not print the disk" << std::endl;
+  }
+}
---
+++ src/Utils/PadronShell.hpp
@@ -0,0 +1,48 @@
+#ifndef PADRON_SHELL_HPP
+#define PADRON_SHELL_HPP
+
+#include "PadronClient.hpp"
+
+#include <string>
+
+
+#define CREATE_COMMAND  "create"
+#define WRITE_COMMAND   "write"
+#define READ_COMMAND    "read"
+#define SEARCH_COMMAND  "search"
+#define PRINT_COMMAND   "print"
+#define UPDATECODE_COMMAND  "code"
+#define VERIFYCODE_COMMAND  "verifycode"
+#define VERIFYCARNET_COMMAND "verifycarnet"
+#define UPDATEVOTE_COMMAND "vote"
+
+class PadronShell {
+ private:
+  PadronClient& padronClient;
+
+
+ public:
+  PadronShell(PadronClient& padronClient);
+  ~PadronShell();
+
+ private:
+
+  // code created successfully (actualizar padron)
+  void updateCode(const std::string& filepath, std::string codigo);
+  // code associated with carnet verified against padron (verificar contra padron)
+  void verifyCode(const std::string& filepath, std::string codigo);
+  // voter carnet verified against padron (verificar carnet para crear codigo)
+  void verifyCarnet(const std::string& filepath);
+  // voter vote updated in padron (actualizar padron)
+  void updateVote(const std::string& filepath);
+  void printDisk();
+
+ private:
+  void parse(const std::string& input);
+
+ public:
+  void shell();
+};
+
+
+#endif
