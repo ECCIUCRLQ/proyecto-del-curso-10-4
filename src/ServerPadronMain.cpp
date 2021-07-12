95101aa5067fad4f554c9d3d681ea95a8b6ae7e9
Sebas7g7 <42952841+Sebas7g7@users.noreply.github.com>
2021-07-12 17:34:14 -0600

    .

--- src/ServerPadronMain.cpp
+++ src/ServerPadronMain.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[]) {
   PadronServer server(fs, "FACU_INGE");
 
   // Add clients
-  server.addClient("172.24.131.223", "8082");#cambio para conexion entre maquinas
+  server.addClient("172.24.131.223", "8082");//cambio para conexion entre maquinas
   server.addClient("127.0.0.1", "8083");
 
   server.listenForever("8081");
