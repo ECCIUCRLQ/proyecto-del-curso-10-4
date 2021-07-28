c532d0ae5e088bd9945233be255a5f3deec4c8f4
Sebas7g7 <42952841+Sebas7g7@users.noreply.github.com>
2021-07-28 02:41:46 -0600

    .

--- src/Logger/Logger.cpp
+++ src/Logger/Logger.cpp
@@ -1,11 +1,18 @@
+/* COPYRIGHT Equipo Tronaditas 2021 */
+// Clase para creacion de la bitacora
 #include <iostream>
 #include <fstream>
 #include <string>
 #include "Logger.hpp"
-
+	/*
+	* datos de la funcion log
+	* @param method-metodo que se esta verificando
+	* @param information- informacion retornada por el metodo
+	* @param success-booleano de verificacion
+	*/
 void Logger::log (std::string method, std::string information, int success) {
-	 	std::ofstream logFile;
-	 	logFile.open("Logger.txt",std::ios::app);
+	std::ofstream logFile;
+	logFile.open("Logger.txt",std::ios::app);
 	if (success == 1){
 		logFile << "Invocación válida de método "<< method <<": " << information << "\n\n";
 	} else {
