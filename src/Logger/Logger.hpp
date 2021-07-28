e37089b186722dfc5633bf749722077b0f259dbb
Sebas7g7 <42952841+Sebas7g7@users.noreply.github.com>
2021-07-28 02:39:59 -0600

    .

--- src/Logger/Logger.hpp
+++ src/Logger/Logger.hpp
@@ -1,3 +1,5 @@
+/* COPYRIGHT Equipo Tronaditas 2021 */
+// Clase para creacion de la bitacora
 #ifndef LOGGER_H
 #define LOGGER_H
 
@@ -6,7 +8,12 @@
 #include <iostream>
 #include <string>
 class Logger {
-
+	/*
+	* datos de la funcion log
+	* @param method-metodo que se esta verificando
+	* @param information- informacion retornada por el metodo
+	* @param success-booleano de verificacion
+	*/
 
 	public:
 	 void log(std::string method, std::string information, int success);
